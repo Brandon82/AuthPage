fd64c4013946ea68d9b711276a6c002a99151603
Brandon82 <83241992+Brandon82@users.noreply.github.com>
2022-09-23 00:13:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import dearpygui.dearpygui as dpg
 from dearpygui.demo import show_demo
 from licensing.models import *
 from licensing.methods import Key, Helpers
-import os
+import time
 import ctypes
 import sys
-import requests
+#import requests
 import urllib.request as urllib2
 
 WIN_WIDTH = 500
@@ -57,14 +57,14 @@ def login_cb(s,d):
 
     if result[0] == None or not Helpers.IsOnRightMachine(result[0], v=2):
         dpg.show_item(auth_fail_text)
+        time.sleep(2)
+        dpg.destroy_context()
+    else:
+        dpg.show_item(auth_pass_text)
+        time.sleep(.5)
         dpg.delete_item(item=main_win)
         dpg.show_item(item=win2)
         dpg.set_primary_window(win2, True)
-
-        #ctypes.windll.user32.MessageBoxW(0, "Failed", "Auth", 0)
-    else:
-        pass
-        #dpg.show_item(auth_pass_text)
         #ctypes.windll.user32.MessageBoxW(0, "Success", "Auth", 0)
 
 
@@ -74,9 +74,8 @@ with dpg.window(width = WIN_WIDTH, height = WIN_HEIGHT, no_title_bar=True, no_re
     local_key = dpg.add_input_text(label='Auth key:', callback=key_cb)
     dpg.add_button(label='Login', callback=login_cb)
 
-
-    auth_fail_text = dpg.add_text('Failed', show=False, tag='a')
-    auth_pass_text = dpg.add_text('Success', show=False, tag='b')
+    auth_fail_text = dpg.add_text('Login Failed', show=False)
+    auth_pass_text = dpg.add_text('Success', show=False)
 
 with dpg.window(width = WIN_WIDTH, height = WIN_HEIGHT, no_title_bar=True, no_resize=True, no_move=True, show=False) as win2:
     title_text = dpg.add_text('welcome')
